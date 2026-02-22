7995bbcf244ef1c73b963918358fc3b46a471294
Ethan Evans <ethanevans.j@gmail.com>
2026-02-22 00:20:19 -0600

    return (incomplete)

--- setup.py
+++ setup.py
@@ -34,6 +34,25 @@ def build_config():
     winners       = int(prompt("winners"))
     total_seats   = num_districts * winners
 
+    return {
+        "run_name":                run_name,
+        "geodata_path":            geodata_path,
+        "gerrychain_output_dir":   f"outputs/districts/{run_name}",
+        "population_column":       population_column,
+        "pop_of_interest_column":  pop_of_interest_col,
+        "chain_length":            chain_length,
+        "num_subsamples":          num_subsamples,
+        "total_seats":             total_seats,
+        "district_configs":        [{"num_districts": num_districts, "winners": winners}],
+        "turnout":                 turnout,
+        "num_voters":              num_voters,
+        "slate_to_candidates":     slate_to_candidates,
+        "focal_group":             focal_group,
+        "cohesion_parameters":     cohesion_parameters,
+        "alphas":                  alphas,
+        "num_reps":                num_reps,
+    }
+
 if __name__ == "__main__":
 
     name = input("Use existing config file? (y/n): ")
