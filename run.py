a5eaade963d42efb6141a0aa9e32de7d584fae05
Sarah Song <sarahsong@Sarahs-MacBook-Pro.local>
2026-02-27 10:06:18 -0600

    clean up run.py

--- run.py
+++ run.py
@@ -1,9 +1,3 @@
-import json
-import geopandas as gpd
-from pathlib import Path
-import jsonlines as jl
-from tqdm import tqdm
-from gerrychain import Graph
 from pipeline.settings_generator import generate_settings
 from pipeline.profile_generator import generate_profiles
 from pipeline.simulate_elections import simulate_elections
